0d1c1f1b0a2a27e171dc91eecbf6e1cfe7bedc5c
Sabardi <Sabardibahari@gmail.com>
2023-09-19 21:33:48 +0800

    first commit

---
+++ class.php
@@ -0,0 +1,33 @@
+<?php
+class Manusia{
+    //properti 
+
+    //method
+    function Makan() {
+        echo "Manusia makan 3 kali dalam sehari <br>";
+    }
+    function Jalan(){
+        echo "jalan setiap hari <br>";
+    }
+    function Pekerjaan(){
+        echo "pekerjaan <br>";
+    }
+}
+$nama = new Manusia();
+echo $nama->Makan();
+echo $nama->Jalan();
+echo $nama->Pekerjaan();
+
+class Buah{
+    function NamaBuah(){
+        echo "Buah buah an <br>";
+    }function WarnaBuah(){
+        echo "Buah buah biasa nya berwarna merah <br>";
+    }
+}   
+
+$namabuah = new Buah();
+$warna = new Buah();
+echo $namabuah->NamaBuah();
+echo $warna->WarnaBuah();
+?>
---
+++ variabel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * x
+ * /
+ * +
+ * -
+ * **
+ */
+$Bilangan1 = 20;
+$Bilangan2 = 40;
+$Hasil = $Bilangan1 + $Bilangan2;
+$Hasil2 = $Bilangan1 - $Bilangan2;
+$Hasil3 = $Bilangan1 * $Bilangan2;
+$Hasil4 = $Bilangan1 / $Bilangan2;
+$Hasil5 = $Bilangan1 % $Bilangan2;
+$Hasil6 = $Bilangan1 ** $Bilangan2;
+
+echo "hasil perhitungan $Bilangan1 + $Bilangan2 nya = $Hasil <br>";
+echo "hasil perhitungan $Bilangan1 - $Bilangan2 nya = $Hasil2 <br>";
+echo "hasil perhitungan $Bilangan1 x $Bilangan2 nya = $Hasil3 <br>";
+echo "hasil perhitungan $Bilangan1 / $Bilangan2 nya = $Hasil4 <br>";
+echo "hasil perhitungan $Bilangan1 % $Bilangan2 nya = $Hasil5 <br>";
+echo "hasil perhitungan $Bilangan1 ** $Bilangan2 nya = $Hasil6 <br>";
+?>
