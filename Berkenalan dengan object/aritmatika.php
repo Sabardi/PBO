49302bc621e825a684c48ceb07676b0a7b1036dc
Sabardi <Sabardibahari@gmail.com>
2023-09-25 21:11:25 +0800

    aritmatika problem

---
+++ Berkenalan dengan object/aritmatika.php
@@ -0,0 +1,25 @@
+<?php
+/** todo
+ * buatlah object matika 
+ * buatlah properti angka1 dan angka2 dan hasil
+ * buatlah methot penjumlahan dan hasil
+ */
+class matika{
+    public $angka1;
+    public $angka2;
+    public $hasil;
+
+    function penjumlahan() {
+        $this->angka1 + $this->angka2;
+    }
+    function hasil(){
+        return "hasil".$this->hasil();
+    }
+}
+$penjumlahan = new matika();
+$penjumlahan->angka1 = 10;
+$penjumlahan->angka2 = 10;
+
+echo $penjumlahan->penjumlahan()
+
+?>
