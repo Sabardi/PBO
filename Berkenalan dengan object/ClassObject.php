d52cc3b50cf0ed1577050acf60613be8fd83f6f9
Sabardi <Sabardibahari@gmail.com>
2023-09-25 14:52:11 +0800

    Membuat objet dan membedakan properti dan method

--- Berkenalan dengan object/ClassObject.php
+++ Berkenalan dengan object/ClassObject.php
@@ -1,22 +1,38 @@
 <?php 
 //object dasar start
- class Matimatika {
 
+class Matimatika {
     function perkalian(){
         echo "perkalian <br>";
     }
-
-    function pembagian(){
-        echo "pembagian";
-    }
  }
 $hitungperkalian = new Matimatika();
-$hitungpembagian = new Matimatika();
 echo $hitungperkalian->perkalian();
-echo $hitungpembagian->perkalian();
 
 // Object dasar end
-echo "<br>"; //
+echo "=== <u> Property dan method </u>==== <br>"; //
+//property dan method start
+class product  {
+    
+    // property 
+    public $No = "001"; //dinamakan Access modifier yang bisa di akses darimanapun
+    public $merek = "politron";
+    public $harga = 5000;
+    
+    //method
+    function PesanProduk(){
+            echo "Produk yang di pesan";
+    }
+}
+$tv = new product();
+echo "No : $tv->No";//untuk mengakses property dan method di dalam sebuah object menggunakan (->)tanda panah 
+echo "<br>";
+echo "merek : $tv->merek";
+echo "<br>";
+echo "merek : $tv->harga";
+echo "<br>";
+echo $tv->PesanProduk();//pemangilan method nya
+//property dan method end
 
 
 
