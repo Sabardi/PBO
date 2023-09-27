5cc0c5ca494e734c71b1e8e47dc615ad8dd75206
Sabardi <Sabardibahari@gmail.com>
2023-09-27 08:48:17 +0800

    menambahkan produk tanpa batasan jumlah stok nya

--- Berkenalan dengan object/Soal/soal2.php
+++ Berkenalan dengan object/Soal/soal2.php
@@ -1,73 +1,50 @@
 <?php
-
-use produk as GlobalProduk;
-
-/**
- * saya ingin membuat method tambahstok() ke dalam class Produk.
-Sesuai dengan namanya, method ini dipakai untuk menambah nilai $stok. Method
-tambahstok() nantinya bisa menerima 1 argumen, yakni jumlah stok yang akan ditambah.
-Jika method ini dipanggil tanpa argument, maka dianggap stok bertambah 1 lusin (12
-buah).
-Karena ukuran gudang yang terbatas, $stok dibatasi maksimal 100 barang. Jika method
-tambahstok() dipanggil dan ternyat
- */
-class produk{
+class Produk{
+    // properti
     public $jenis;
     public $merek;
     public $stok;
 
-    function Pesanproduk(){
-        $this->stok = $this->stok -1;
+    //method
+    function pesanProduk(){
+        $this->stok = $this->stok -1;// stok sama dengan stok untuk mengurangi jumlah stok nya
+    }
+    
+    function BorongProduk($jumlah = 10){ //$jumlah merupakan parameter //meskipun di berikan nilai deful yang akan tetep di jalankan ialah parameter yang di isi
+        $this->stok = $this->stok - $jumlah;
     }
 
-    function Borongproduk($barang){
-        $this->stok = $this->stok -$barang;
+    function cekStok(){
+        return " sisa stok = ".$this->stok. "<br>";
     }
 
-    function Stokproduk(){
-        return " sisa stok ".$this->stok;
+    function TambahProduk($jumlah){
+        $this->stok = $this->stok + $jumlah;
     }
+ }
 
-    function TambahProduk($barang){
-        $totalstok = $this->stok +$barang;
-        // if ($totalstok <= 100) {
-        //     $this->stok = $totalstok;
-        //     $pesan = "stok berhasil ditambah <br>";
-        //     $pesan = " Jumlah stok saat ini : ".$this->stok." <br>";
-        // }
-        // else{
-        //     $pesan = "Maaf, stok sudah penuh. Penambahan stok dibatalkan <br>"; 
-        //     $pesan = "Jumlah stok saat ini: ".$this->stok."<br>"; 
-        // }
-        // return $pesan;
-    }   
-
-}
-
-$produk1 = new Produk();
-$produk1->jenis = "motor";
-$produk1->merek = "supra";
-$produk1->stok = 100;
-
-echo $produk1->pesanProduk();
-echo $produk1->pesanProduk();
-echo $produk1->pesanProduk();
-
-//borong produk
-echo $produk1->BorongProduk(70);
-echo $produk1->Stokproduk();
-
-//tambah stok
-$produk1->jenis = "mesin cuci";
-$produk1->merek = "samsung";
-$produk1->stok = 100;
-
-echo $produk1->TambahProduk(10);
-echo $produk1->TambahProduk(30);
-echo $produk1->TambahProduk(30);
-echo $produk1->Stokproduk();
-echo $produk1->TambahProduk(100);//gagal masuk ke stok
-
-
-
+ $motor = new Produk();
+ $motor->jenis = "motor";
+ $motor->merek = "supra";
+ $motor->stok = 100;
+ 
+ echo "Jumlah produk : $motor->stok <br>";
+ echo $motor->pesanProduk();
+ echo $motor->pesanProduk();
+ echo $motor->pesanProduk();
+ echo $motor->pesanProduk();
+ echo $motor->cekStok();//sisa dari produk setelah di panggil beberapa kali
+ 
+ /*membuat method
+ borongProduk(). Jika pada pesanProduk() hanya bisa memesan 1 buah produk saja, pada
+ method borongProduk() ini bisa membeli banyak produk sekaligus. Jumlah produk yang dibeli
+ nantinya akan menjadi sebuah argument*/
+ 
+ echo "sisa motor di Borong ".$motor->BorongProduk(10);// sisa stok nya
+ echo "sisa motor di Borong ".$motor->BorongProduk(40);// sisa stok nya
+ echo "sisa motor di Borong ".$motor->BorongProduk(10);// sisa stok nya
+ echo $motor->cekStok();
+ 
+ $motor ->TambahProduk(10);// penambahan produk
+ echo $motor->cekStok();
 ?>
