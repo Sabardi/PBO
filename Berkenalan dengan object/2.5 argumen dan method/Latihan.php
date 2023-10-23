1ac4fb2e6295ce09af82c0eade13ae2dc69c9634
Udinsafar <Udinsafar1947@gmail.com>
2023-10-24 07:39:29 +0800

    Rename file

---
+++ Berkenalan dengan object/2.5 argumen dan method/Latihan.php
@@ -0,0 +1,14 @@
+<!-- # Exercise
+ Latihan ini adalah tentang pembuatan method dengan argument, tapi saya ingin sedikit 
+lebih kompleks.
+ Kasusnya seperti ini, saya ingin membuat method tambahstok() ke dalam class Produk. 
+Sesuai dengan namanya, method ini dipakai untuk menambah nilai $stok. Method 
+tambahstok() nantinya bisa menerima 1 argumen, yakni jumlah stok yang akan ditambah.
+ Jika method ini dipanggil tanpa argument, maka dianggap stok bertambah 1 lusin (12 
+buah).
+ Karena ukuran gudang yang terbatas, $stok dibatasi maksimal 100 barang. Jika method 
+tambahstok() dipanggil dan ternyata itu akan melewati 100 stok, batalkan penambahan 
+dan tampilkan pesan kesalahan. -->
+<?php
+
+?>
